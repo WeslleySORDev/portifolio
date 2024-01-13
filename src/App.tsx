f8a99ed1aa05c4a7e8cdf5d519793938a3e764b8
WeslleySORDev <weslleysordev@gmail.com>
2024-01-13 15:32:42 -0300

    feat: header base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <header className="flex justify-end items-center px-4 py-2 gap-4">
+        <ul className="flex items-center gap-4">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Começo</a>
+          </li>
+          <li>
+            <a href="#">Fim</a>
+          </li>
+        </ul>
+        <ModeToggle />
+      </header>
+      <main></main>
     </>
   );
 }
