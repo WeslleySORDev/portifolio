9224a2b4c4a6108a084ef7a4f3fbf8ed6c5b5783
WeslleySORDev <weslleysordev@gmail.com>
2024-01-14 17:02:38 -0300

    feat: header style

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,29 @@ import { ModeToggle } from "./components/mode-toggle";
 function App() {
   return (
     <>
-      <header className="flex items-center justify-end gap-4 px-4 py-2">
+      <header className="sticky top-0 flex items-center justify-end gap-32 border-b border-border/40 px-4 py-2">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <a href="#">
+              <a className="hover:brightness-75" href="#about-me">
                 <NavigationMenuLink>Sobre mim</NavigationMenuLink>
               </a>
             </NavigationMenuItem>
             <Separator orientation="vertical" />
             <NavigationMenuItem>
-              <a href="#">
+              <a className="hover:brightness-75" href="#projects">
                 <NavigationMenuLink>Projetos</NavigationMenuLink>
               </a>
             </NavigationMenuItem>
             <Separator orientation="vertical" />
             <NavigationMenuItem>
-              <a href="#">
+              <a className="hover:brightness-75" href="#stacks">
                 <NavigationMenuLink>Stacks</NavigationMenuLink>
               </a>
             </NavigationMenuItem>
             <Separator orientation="vertical" />
             <NavigationMenuItem>
-              <a href="#">
+              <a className="hover:brightness-75" href="#contact">
                 <NavigationMenuLink>Contato</NavigationMenuLink>
               </a>
             </NavigationMenuItem>
